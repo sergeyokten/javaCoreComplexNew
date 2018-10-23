928f8f0a3bfea1d2c0decab54f04d8ad87979c5e
sergeyokten <sergey.okten@gmail.com>
2018-10-23 13:14:21 +0300

    add l16

---
+++ src/lesson15/Main.java
@@ -0,0 +1,85 @@
+package lesson15;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        ArrayList<String> strings = new ArrayList<>();
+//        strings.add("oktenweb1");
+//        strings.add("oktenweb2");
+//        strings.add("oktenweb3");
+//        strings.add("oktenweb4");
+//        strings.add("oktenweb5");
+//
+//        System.out.println(strings.size());
+//        System.out.println(strings);
+//
+////        strings.clear();
+//        strings.remove(new String("oktenweb1"));
+//        System.out.println(strings);
+//        strings.remove(0);
+//        System.out.println(strings);
+//
+//        ArrayList<String> stringListNew = new ArrayList<>();
+//        stringListNew.add("hello1");
+//        stringListNew.add("hello2");
+//        stringListNew.add("hello3");
+//
+//        strings.addAll(stringListNew);
+//        System.out.println(strings);
+//
+//        ArrayList<String> thingsToDelete = new ArrayList<>();
+//        thingsToDelete.add("hello11");
+//        thingsToDelete.add("oktenweb15");
+//
+//
+//        strings.removeAll(thingsToDelete);
+//        System.out.println(strings);
+//
+//        System.out.println(thingsToDelete);
+//
+//        String s = strings.get(3);
+//        System.out.println(s);
+//
+//        System.out.println("---------------");
+////        for (String string : strings) {
+////            if (string.equals("hello1")) {
+////                strings.remove(string);
+////            }
+////
+////
+////        }
+//
+//        Iterator<String> iterator = strings.iterator();
+//
+//        while (iterator.hasNext()) {
+//            String currentString = iterator.next();
+//            if (currentString.equals("hello1")) {
+//                iterator.remove();
+//            }
+//        }
+//
+//        System.out.println(strings);
+//
+//        System.out.println(strings.contains("oktenweb5"));
+//
+//        System.out.println(strings.containsAll(thingsToDelete));
+//        strings.set(1, "asdfasdghfash");
+//        System.out.println(strings);
+//        strings.add(0, "qwqweqwe");
+//        System.out.println(strings);
+//        List<String> subList = strings.subList(0, 3);
+//        System.out.println(subList);
+//
+//
+//        List<String> linkedList = new LinkedList<>(strings);
+//        System.out.println(linkedList);
+//        linkedList;
+
+        List<User> users = new ArrayList<>();
+
+
+    }
+}
